78787e0cd1456f07f62bd9fdcc12f5f4174ee985
Erlend Hapnes <erlend_hapnes@hotmail.com>
2022-01-12 09:46:57 +0100

    Added count

--- www-1.tf
+++ www-1.tf
@@ -1,6 +1,8 @@
 resource "digitalocean_droplet" "www-1" {
+    count = 4
+    
     image = "ubuntu-20-04-x64"
-    name = "www-1"
+    name = "ubuntu-${count.index}"
     region = var.region
     size = "s-1vcpu-1gb"
     ipv6 = true
