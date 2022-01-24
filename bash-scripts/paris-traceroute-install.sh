9f540c81b66a85fc3e6a80d9ee45f2812a7a28e1
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-01-24 11:24:56 +0100

    Added bash scripts

---
+++ bash-scripts/paris-traceroute-install.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+mkdir ~/git
+cd ~/git
+git clone https://github.com/meeps44/libparistraceroute.git
+cd ~/git/libparistraceroute
+mkdir m4
+./autogen.sh
+./configure
+ldconfig
+make
+make install
+export PATH=$PATH:~/git/libparistraceroute/paris-traceroute
+ldconfig
