48fcd3f1db202c0c5a72e423b6c06f8b6fc99473
Erlend Hapnes <erlend_hapnes@hotmail.com>
2022-01-31 16:05:05 +0100

    Added destination address dictionary element

--- python-scripts/file-to-list.py
+++ python-scripts/file-to-list.py
@@ -4,6 +4,11 @@ with open("/Users/admin/Desktop/test-new.txt", "r") as my_file:
     hop_list = my_file.read().splitlines() 
     # print(hop_list)
 
+# Remove first item in the list (the destination address) and add it as separate dictionary element
+dest = hop_list.pop(0)
+dest_dict = { "destination": dest}
+print(dest_dict)
+
 count = 0 # in case items is empty and you need it after the loop
 hop_dictionary = { count : item for count, item in enumerate(hop_list, start=1) }
 # hop_dictionary = { hop : "hop" for hop in hop_list }
