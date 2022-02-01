09baed6c4d9e22bc3058aef17f33e60b9b74dd47
Erlend Hapnes <erlend_hapnes@hotmail.com>
2022-02-01 13:19:51 +0100

    Added option to print routes to stdout

--- python-scripts/route-compare.py
+++ python-scripts/route-compare.py
@@ -5,21 +5,47 @@ import argparse, json
 parser = argparse.ArgumentParser()
 parser.add_argument("file1")
 parser.add_argument("file2")
+parser.add_argument('-v', '-V', '--v', '--V', action='store_true') 
+# if the -v flag is enabled, dump both routes to stdout
+# action='store_true' implies default=False. Conversely, you could have action='store_false', which implies default=True.
 args = parser.parse_args()
+
 
 with open(args.file1, "r") as file1, open(args.file2, "r") as file2:
     # returns JSON object as a dictionary
     data1 = json.load(file1)
     data2 = json.load(file2)
     
-    result = data1['hops']['2'] == data2['hops']['2']
+    # result = data1['hops']['2'] == data2['hops']['2']
 
     for key, value in data1['hops'].items():
         # print(f"{key}:{value}")
         try:
+            if (data1['hops'][key] != data2['hops'][key]):
+                print("Route difference detected!")
+                result = False
+                break
             result = data1['hops'][key] == data2['hops'][key]
         except KeyError:
+            print("KeyError")
             result = False
             break
     
-    print(result)
+    if (result):
+        print("The routes are equal")
+        if (args.v):
+            print("Route 1: ")
+            for k, v in data2['hops'].items():
+                print(f"{k}:{v}")
+            print("Route 2: ")
+            for k, v in data1['hops'].items():
+                print(f"{k}:{v}")
+    else:
+        print("The routes are not equal")
+        if (args.v):
+            print("Route 1: ")
+            for k, v in data2['hops'].items():
+                print(f"{k}:{v}")
+            print("Route 2: ")
+            for k, v in data1['hops'].items():
+                print(f"{k}:{v}")
