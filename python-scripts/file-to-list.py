bc6b0afff13168d1e70ea8958a90163c6af64421
Erlend Hapnes <erlend_hapnes@hotmail.com>
2022-02-01 10:14:23 +0100

    Added uuid and argparse

--- python-scripts/file-to-list.py
+++ python-scripts/file-to-list.py
@@ -1,24 +1,28 @@
+import json, uuid, argparse
 
-with open("/Users/admin/Desktop/test-new.txt", "r") as my_file:
-    # hop_list = my_file.readlines()
+parser = argparse.ArgumentParser()
+parser.add_argument("filepath")
+args = parser.parse_args()
+
+# "/Users/admin/Desktop/test-new.txt"
+
+with open(args.filepath, "r") as my_file:
     hop_list = my_file.read().splitlines() 
-    # print(hop_list)
-
 
 # Remove first item in the list (the destination address) and add it as separate dictionary element
 dest = hop_list.pop(0)
 dest_dict = { "destination": dest}
-# print(dest_dict)
 
 # Create top-level dictionary
 my_dict = {}
+my_dict["probe_uuid"] = str(uuid.uuid4())
+# my_dict["flow_label"] = 100
 my_dict["destination"] = dest
 
 count = 0 # in case items is empty and you need it after the loop
 hop_dictionary = { count : item for count, item in enumerate(hop_list, start=1) }
-# print(hop_dictionary)
 
 my_dict["hops"] = hop_dictionary
-print(my_dict)
-# hop_dictionary = { hop : "hop" for hop in hop_list }
-# fruit_dictionary = { fruit : "In stock" for fruit in fruits }
+
+with open('app.json', 'w') as fp:
+    json.dump(my_dict, fp, indent=4)
