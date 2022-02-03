38afaa039c6c1b7a0c5ba0f95837a26df69e92ba
Erlend <erlend_hapnes@hotmail.com>
2022-02-03 23:04:59 +0100

    Updated hostname

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -2,14 +2,14 @@
 
 # date=$(date '+%Y-%m-%d')
 date=$(date '+%Y-%m-%d %H:%M:%S')
-local_hostname="$HOSTNAME"
+hostname="$HOSTNAME"
 # flow_labels=(0 23 100 150 200)
 flow_labels=(23 100)
 # destination_port="default"
 
-if [ $local_hostname == $somehost ]
+if [ "$hostname" == "$somehost" ]
 then
-    echo $local_hostname
+    echo $hostname
     # f
 elif [ ]
 then
@@ -44,7 +44,7 @@ for flow_label in "${flow_labels[@]}"; do
     done
     python3 file-to-list.py $filename # run script and get json-file. json-file is then sent to logserver via scp
     # on logserver we can then compare the files and log the result of the comparison
-    scp -i ~/.ssh/scp-key $filename 209.97.138.74:/root/logs/'$test-logs-january'/
+    scp -i ~/.ssh/scp-key $filename 209.97.138.74:/root/logs/${hostname}/
 done
 
 # TODO: send $filename to log server via scp
