d31304052fc1f53358ba1a9626d5465e9f7ad662
Erlend <erlend_hapnes@hotmail.com>
2022-02-03 23:15:19 +0100

    Removed hostname variable and array

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -1,40 +1,40 @@
 #!/usr/bin/env bash
 
-hosts=("ubuntu-lon1-0" "ubuntu-ams3-0") # bash array of strings (hostnames)
+#hosts=("ubuntu-lon1-0" "ubuntu-ams3-0") # bash array of strings (hostnames)
 
 # date=$(date '+%Y-%m-%d')
 date=$(date '+%Y-%m-%d %H:%M:%S')
-hostname="$HOSTNAME"
+#hostname="$HOSTNAME"
 # flow_labels=(0 23 100 150 200)
 flow_labels=(23 100)
 # destination_port="default"
 
-if [ "$hostname" == "$somehost" ]
-then
-    echo $hostname
+#if [ hosts[0] = $HOSTNAME ]
+#then
+    #echo $hostname
     # f
-elif [ ]
-then
-
-elif [ ]
-then
-
-elif [ ]
-then
-
-elif [ ]
-then
-
-elif [ ]
-then
-
-elif [ ]
-then
-
-elif [ ]
-then
-
-fi
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#elif [ ]
+#then
+#
+#fi
 
 for flow_label in "${flow_labels[@]}"; do
 
@@ -46,7 +46,7 @@ for flow_label in "${flow_labels[@]}"; do
     done
     python3 file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
     # on logserver we can then compare the files and log the result of the comparison
-    scp -i ~/.ssh/scp-key ${filename} 209.97.138.74:/root/logs/${hostname}/
+    scp -i ~/.ssh/scp-key ${filename} 209.97.138.74:/root/logs/${HOSTNAME}/
 done
 
 # TODO: send $filename to log server via scp
