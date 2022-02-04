4cf6aea1d236faab8219aa3e738d55419346be5d
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-02-04 12:45:07 +0100

    Fixes

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -3,7 +3,6 @@
 #hosts=("ubuntu-lon1-0" "ubuntu-ams3-0") # bash array of strings (hostnames)
 
 # date=$(date '+%Y-%m-%d')
-date=$(date '+%Y-%m-%d %H:%M:%S')
 #hostname="$HOSTNAME"
 # flow_labels=(0 23 100 150 200)
 flow_labels=(23 100)
@@ -11,11 +10,12 @@ flow_labels=(23 100)
 
 for flow_label in "${flow_labels[@]}"; do
 
-    cat ipv6-address-list.txt | while read line; do
+    cat /root/ipv6-address-list.txt | while read line; do
+        date=$(date '+%Y-%m-%d-%H:%M:%S')
         filename="$HOSTNAME-${date}-${flow_label}.txt"
         echo "filename: $filename"
         destination_address=$line
-        sudo paris-traceroute -T ${flow_label} ${destination_address} >> ${filename}
+        sudo paris-traceroute -T "${flow_label}" "${destination_address}" >> $filename
         #python3 /root/file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
         python3 file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
         # on logserver we can then compare the files and log the result of the comparison
