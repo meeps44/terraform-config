4a447ef5ad717445ec30a8d045fa5e024a1e097c
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-02-04 11:52:32 +0100

    Small update

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -9,33 +9,6 @@ date=$(date '+%Y-%m-%d %H:%M:%S')
 flow_labels=(23 100)
 # destination_port="default"
 
-#if [ hosts[0] = $HOSTNAME ]
-#then
-    #echo $hostname
-    # f
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#elif [ ]
-#then
-#
-#fi
-
 for flow_label in "${flow_labels[@]}"; do
 
     cat ipv6-address-list.txt | while read line; do
@@ -43,10 +16,11 @@ for flow_label in "${flow_labels[@]}"; do
         echo "filename: $filename"
         destination_address=$line
         sudo paris-traceroute -T ${flow_label} ${destination_address} >> ${filename}
+        #python3 /root/file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
+        python3 file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
+        # on logserver we can then compare the files and log the result of the comparison
+        scp -i ~/.ssh/scp-key ${filename} 209.97.138.74:/root/logs/${HOSTNAME}/
     done
-    python3 file-to-list.py ${filename} # run script and get json-file. json-file is then sent to logserver via scp
-    # on logserver we can then compare the files and log the result of the comparison
-    scp -i ~/.ssh/scp-key ${filename} 209.97.138.74:/root/logs/${HOSTNAME}/
 done
 
 # TODO: send $filename to log server via scp
