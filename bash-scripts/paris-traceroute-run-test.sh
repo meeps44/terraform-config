9f814c3174cea65addb8ccbe64f5c4ceb041bb27
Erlend <erlend_hapnes@hotmail.com>
2022-02-06 23:31:06 +0000

    More fixes

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -13,7 +13,7 @@ for flow_label in "${flow_labels[@]}"; do
     cat /root/git/terraform-config/bash-scripts/ipv6-address-list.txt | while read line; do
         destination_address=$line
         sudo paris-traceroute -T "${flow_label}" "${destination_address}" > tmp.txt
-        date=$(date '+%Y-%m-%d-%H:%M:%S')
+        date=$(date '+%Y-%m-%d-%H-%M-%S')
         filename="$HOSTNAME-${date}-${flow_label}.txt"
         grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])) | \* \* \*)" tmp.txt >> $filename
         python3 /root/git/terraform-config/python-scripts/file-to-list.py ${filename} # run script and create json-file. json-file is then sent to logserver via scp
