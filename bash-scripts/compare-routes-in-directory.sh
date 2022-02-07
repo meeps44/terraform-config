1fc9be27f2bdfa5831a258d2b881870208d2a2d9
Erlend Hapnes <erlend_hapnes@hotmail.com>
2022-02-08 00:11:00 +0100

    Fixed for-loop and comparator

--- bash-scripts/compare-routes-in-directory.sh
+++ bash-scripts/compare-routes-in-directory.sh
@@ -3,10 +3,12 @@
 # ref: https://stackoverflow.com/questions/20796200/how-to-loop-over-files-in-directory-and-change-path-and-add-suffix-to-filename
 
 directory=$1
+# comparator = the base file we want to compare against
+comparator=$2
 
 for filename in ${directory}/*.json; do
-    firstFile=$filename
-    # [ -e "$filename" ] || continue
-    [ python3 /root/git/terraform-config/python-scripts/route-compare.py "$firstFile" "$filename" ] || continue
-    # ... rest of the loop body
+    # Check if file exists, if yes continue:
+    [ -e "$filename" ] || continue
+    # Run command
+    python3 /root/git/terraform-config/python-scripts/route-compare.py "$comparator" "$filename"
 done
