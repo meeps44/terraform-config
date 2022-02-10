5c6535be4b3638d80ecb0b3b05c4c146710c58f6
Erlend <erlend_hapnes@hotmail.com>
2022-02-10 23:46:30 +0100

    Fixed path

--- bash-scripts/compare-routes-in-directory.sh
+++ bash-scripts/compare-routes-in-directory.sh
@@ -6,12 +6,11 @@ directory=$1
 # comparator = the base file we want to compare against
 comparator=$2
 
-for filename in ${directory}/*.json; do
+for filename in ${directory}*.json; do
     # Check if file exists, if yes continue:
     [ -e "$filename" ] || continue
-    echo $filename
     # Run command
-    python3 /home/erlend/git/terraform-config/python-scripts/route-compare-2.py "$comparator" "$filename"
+    python3 /home/erlend/git/terraform-config/python-scripts/route-compare-2.py "${directory}/$comparator" "$filename"
     #python3 /Users/admin/git/terraform-config/python-scripts/route-compare-2.py "$comparator" "$filename"
 
     #python3 /root/git/terraform-config/python-scripts/route-compare.py "$comparator" "$filename"
