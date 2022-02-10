bdf262f36535a1ef405bbb1bee306dc4db3d8666
Erlend <erlend_hapnes@hotmail.com>
2022-02-10 20:02:36 +0100

    Fixed for-loop

--- bash-scripts/paris-traceroute-run.sh
+++ bash-scripts/paris-traceroute-run.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
-date=$(date '+%Y-%m-%d')
-# date=$(date '+%Y-%m-%d %H:%M:%S')
-host="$HOSTNAME"
-# flow_labels=(0 23 100 150 200)
-flow_labels=(23 100)
-# destination_port="default"
+# get destination tcp-port from input args
+destination_port=$1 
+flow_labels=(230 44)
+host_ip=$(hostname -I | grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])))")
 
-for flow_label in "${flow_labels[@]}"; do
-    filename="$HOSTNAME-${date}-${flow_label}.txt"
-    # echo "filename: $filename"
+while IFS="" read -r p || [ -n "$p" ]
+do
+    for flow_label in "${flow_labels[@]}"; do
+		destination_address=$line
+        hash=$(echo -n ${destination_address} | md5sum | awk '{print $1}')
+        short="${hash:0:6}"
+        sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > tmp.txt
+        date=$(date '+%d-%H-%M-%S')
+        filename="$HOSTNAME-${date}-${short}.txt"
+        #grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])) | \* \* \*)" tmp.txt >> $filename
+        grep -o -E "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))" tmp.txt >> $filename
+        echo "$filename"
+        echo "$destination_address"
+        python3 ~/git/terraform-config/python-scripts/text-to-json.py ../bash-scripts/${filename} ${destination_port} ${host_ip} ${flow_label}
 
-    cat ipv6-address-list.txt | while read line; do
-        destination_address=$line
-        sudo paris-traceroute -T $flow_label $destination_address >> $filename
+        filename="$HOSTNAME-${date}-${short}.json"
+        echo "$filename"
+        # Create directory:
+        ssh -i ~/.ssh/scp-key 209.97.138.74 "mkdir -p /root/logs/${HOSTNAME}/${short}"
+        scp -i ~/.ssh/scp-key ../bash-scripts/${filename} 209.97.138.74:/root/logs/${HOSTNAME}/${short}
     done
-    scp -i ~/.ssh/scp-key $filename 209.97.138.74:/root/logs/test-logs-january/
-done
-
-# TODO: send $filename to log server via scp
+done < /root/git/terraform-config/bash-scripts/ipv6-address-list.txt
