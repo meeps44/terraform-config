9f48b3e40c5d14e1830872fbb3a12c8e9c22df0f
Erlend <erlend_hapnes@hotmail.com>
2022-02-14 11:47:12 +0100

    Fixes

--- python-scripts/hexdump-formatting.py
+++ python-scripts/hexdump-formatting.py
@@ -1,28 +1,17 @@
 import re
 
-regex_tmp = "(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+"
+#new_regex = r"((([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n)"
+#regex_tmp = "(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+"
+#regex = "(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+"
 
-reg = r"((([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+([0-9a-fA-F]+))"
-new_regex = r"((([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n)"
+reg = r"((([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+([0-9a-fA-F]+))+"
 pattern = re.compile(reg)
 
-regex = "(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+\n(([0-9a-fA-F]+) )+"
-my_file = "/Users/admin/Dropbox/Skole/example-output-1.txt"
+my_file = "/home/erlend/git/terraform-config/python-scripts/example-output/example-output-1.txt"
+#my_file = "/Users/admin/Dropbox/Skole/example-output-1.txt"
 
 with open(my_file, "r") as file:
     data = file.read()
-    #result = re.search(pattern, data)
-    result = re.findall(pattern, data)
-    print(result[0])
-    
-    #x = re.findall(regex, data)
-    #data.replace(" ", "")
-    #print("Printing data")
-    #print(x[0])
-
-    #string_without_line_breaks = ""
-    #for line in file:
-        #stripped_line = line.rstrip()
-        #string_without_line_breaks += stripped_line
-
-    #print(string_without_line_breaks)
+    a = ["".join(x) for x in re.findall(pattern, data)]
+    # result = re.findall(pattern, data)
+    print(a)
