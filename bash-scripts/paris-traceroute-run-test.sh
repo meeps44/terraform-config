3da2648867f51747fb7e49e4514e0780289530f6
Erlend Hapnes <ekhapnes@ifi.uio.no>
2022-02-15 11:45:32 +0100

    Fixes

--- bash-scripts/paris-traceroute-run-test.sh
+++ bash-scripts/paris-traceroute-run-test.sh
@@ -10,12 +10,14 @@ for flow_label in "${flow_labels[@]}"; do
         destination_address=$line
         hash=$(echo -n ${destination_address} | md5sum | awk '{print $1}')
         short="${hash:0:6}"
-        #sudo paris-traceroute -T "${flow_label}" "${destination_address}" > tmp.txt
-        sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > tmp.txt
-        #date=$(date '+%Y-%m-%d-%H-%M-%S')
         date=$(date '+%d-%H-%M-%S')
-        #filename="$HOSTNAME-${date}-${short}.txt"
         filename="$HOSTNAME-${short}-${date}.txt"
+        #sudo paris-traceroute -T "${flow_label}" "${destination_address}" > tmp.txt
+
+        #sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > tmp.txt
+        sudo paris-traceroute -T -p ${destination_port} "${flow_label}" "${destination_address}" > $filename
+        #date=$(date '+%Y-%m-%d-%H-%M-%S')
+        #filename="$HOSTNAME-${date}-${short}.txt"
         #grep -o -E "((([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])) | \* \* \*)" tmp.txt >> $filename
         #grep -o -E "(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))" tmp.txt >> $filename
         echo "$filename"
