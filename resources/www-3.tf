ed078c363ae4d24fe01b8689ef42c9ef951fbaa7
Erlend <erlend_hapnes@hotmail.com>
2022-06-01 14:48:56 +0200

    Added resources

---
+++ resources/www-3.tf
@@ -0,0 +1,31 @@
+resource "digitalocean_droplet" "www-3" {
+    count = 1
+    
+    image = "ubuntu-20-04-x64"
+    name = "ubuntu-nyc1-${count.index}"
+    region = "nyc1"
+    size = "s-1vcpu-1gb"
+    ipv6 = true
+    ssh_keys = [
+      data.digitalocean_ssh_key.new-key.id
+    ]
+
+connection {
+    host = self.ipv4_address
+    user = "root"
+    type = "ssh"
+    private_key = file(var.pvt_key)
+    timeout = "2m"
+  }
+
+provisioner "remote-exec" {
+    inline = [
+      "export PATH=$PATH:/usr/bin",
+      "sudo apt update -y",
+      "sudo apt upgrade -y",
+      "apt install autoconf build-essential git libtool -y",
+      "curl -L https://bootstrap.saltstack.com -o install_salt.sh",
+      "sudo sh install_salt.sh -A 209.97.138.74"
+    ]
+  }
+}
